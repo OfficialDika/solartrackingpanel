c027797c893e7c431580082747eb953acda2e960
jimmysanjaya <jimmysanjaya79@gmail.com>
2018-07-02 11:17:20 +0700

    monthly ok

--- solartracking/menu/charts/monthly.php
+++ solartracking/menu/charts/monthly.php
@@ -1,9 +1,11 @@
 <?php
 	if(isset($_POST["bulan"])){
 	    $bulan = $_POST["bulan"];
-			// echo $bulan;
+		
+	} else {
+		$bulan = date("Y")."-".date("m");
 	}
-
+	//echo $bulan;
 	?>
 <div class="col-md-12">
 	<div class="box box-default">
@@ -37,7 +39,7 @@
 
 						function drawBackgroundColor() {
 						var data = new google.visualization.DataTable();
-						data.addColumn('datetime', 'Time of Day');
+						data.addColumn('date', 'Date');
 						data.addColumn('number', 'temperature');
 						data.addRows([
 
@@ -55,13 +57,13 @@
 							}else if ($tgl==1) {
 
 							// $query = "SELECT * FROM `mobile` WHERE DATE(`timestamp`) = CURDATE()";
-							$query = "SELECT * FROM `average` WHERE date like '$bulan%'";
+							$query = "SELECT * FROM `average` WHERE date like '".$bulan."%'";
 							}
 							$result = mysqli_query($con, $query)or die("Error: ".mysqli_error($con));
 							$no=0;
 							while($row_tarik = mysqli_fetch_array($result)){
 							    $no++;
-							    $id = $row_tarik['id'];
+							    //$id = $row_tarik['id'];
 							    $suhu = $row_tarik['avg_temp'];
 							    $timestamp  = $row_tarik['date'];
 							      $timestamp1 = explode(" ",$timestamp);
@@ -81,8 +83,8 @@
 						vAxis: {
 						title: 'Temperature (Celcius)'
 						},
+						colors: ['#3b8cbb'],
 						backgroundColor: '#f1f8e9'
-						colors: ['Blue']
 						};
 						var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 						chart.draw(data, options);
@@ -96,7 +98,7 @@
 						google.charts.setOnLoadCallback(drawBackgroundColor);
 						function drawBackgroundColor() {
 						var data = new google.visualization.DataTable();
-						data.addColumn('datetime', 'Time of Day');
+						data.addColumn('date', 'Date');
 						data.addColumn('number', 'voltage');
 						data.addRows([
 						<?php
@@ -112,27 +114,27 @@
 							$query="SELECT * FROM `mobile` WHERE DATE(`timestamp`) = '$tgl' ORDER BY `raspberry`.`id` DESC";
 							}else if ($tgl==1) {
 							// $query = "SELECT * FROM `mobile` WHERE DATE(`timestamp`) = CURDATE()";
-							$query = "SELECT * FROM `mobile` WHERE DATE(timestamp) = CURDATE()";
+							$query = "SELECT * FROM `average` WHERE date like '".$bulan."%'";
 							}
 							$result = mysqli_query($con, $query)or die("Error: ".mysqli_error($con));
 							$no=0;
 							while($row_tarik = mysqli_fetch_array($result)){
 							    $no++;
-							    $id = $row_tarik['id'];
-							    $volt = $row_tarik['volt'];
-							    $timestamp  = $row_tarik['timestamp'];
+							    //$id = $row_tarik['id'];
+							    $volt = $row_tarik['avg_volt'];
+							    $timestamp  = $row_tarik['date'];
 							      $timestamp1 = explode(" ",$timestamp);
 							      $date = $timestamp1[0];
 							      $date1 = explode("-",$date);
 							      $year = $date1[0];
 							      $month = $date1[1]-1;
 							      $day = $date1[2];
-							      $time = $timestamp1[1];
-							      $time1 = explode(":",$time);
-							      $hour = $time1[0]+7;
-							      $minute = $time1[1];
-							      $second = $time1[2];
-							      echo "[new Date( ".$year.", ".$month.", ".$day.", ".$hour.", ".$minute."), ".$volt."],";
+							      //$time = $timestamp1[1];
+							      //$time1 = explode(":",$time);
+							      //$hour = $time1[0]+7;
+							      //$minute = $time1[1];
+							      //$second = $time1[2];
+							      echo "[new Date( ".$year.", ".$month.", ".$day."), ".$volt."],";
 
 							}
 							?>
@@ -144,8 +146,8 @@
 						vAxis: {
 						title: 'Voltage (Volt)'
 						},
+						colors: ['#ffc600'],
 						backgroundColor: '#f1f8e9'
-						colors: ['Yellow']
 						};
 						var chart = new google.visualization.LineChart(document.getElementById('chart_volt'));
 						chart.draw(data, options);
@@ -159,7 +161,7 @@
 						google.charts.setOnLoadCallback(drawBackgroundColor);
 						function drawBackgroundColor() {
 						var data = new google.visualization.DataTable();
-						data.addColumn('datetime', 'Time of Day');
+						data.addColumn('date', 'Date');
 						data.addColumn('number', 'ampere');
 						data.addRows([
 						<?php
@@ -175,27 +177,27 @@
 							$query="SELECT * FROM `mobile` WHERE DATE(`timestamp`) = '$tgl' ORDER BY `raspberry`.`id` DESC";
 							}else if ($tgl==1) {
 							// $query = "SELECT * FROM `mobile` WHERE DATE(`timestamp`) = CURDATE()";
-							$query = "SELECT * FROM `mobile` WHERE DATE(timestamp) = CURDATE()";
+							$query = "SELECT * FROM `average` WHERE date like '".$bulan."%'";
 							}
 							$result = mysqli_query($con, $query)or die("Error: ".mysqli_error($con));
 							$no=0;
 							while($row_tarik = mysqli_fetch_array($result)){
 							    $no++;
-							    $id = $row_tarik['id'];
-							    $ampere = $row_tarik['ampere'];
-							    $timestamp  = $row_tarik['timestamp'];
+							    //$id = $row_tarik['id'];
+							    $ampere = $row_tarik['avg_ampere'];
+							    $timestamp  = $row_tarik['date'];
 							      $timestamp1 = explode(" ",$timestamp);
 							      $date = $timestamp1[0];
 							      $date1 = explode("-",$date);
 							      $year = $date1[0];
 							      $month = $date1[1]-1;
 							      $day = $date1[2];
-							      $time = $timestamp1[1];
-							      $time1 = explode(":",$time);
-							      $hour = $time1[0]+7;
-							      $minute = $time1[1];
-							      $second = $time1[2];
-							      echo "[new Date( ".$year.", ".$month.", ".$day.", ".$hour.", ".$minute."), ".$ampere."],";
+							      //$time = $timestamp1[1];
+							      //$time1 = explode(":",$time);
+							      //$hour = $time1[0]+7;
+							      //$minute = $time1[1];
+							      //$second = $time1[2];
+							      echo "[new Date( ".$year.", ".$month.", ".$day."), ".$ampere."],";
 							}
 							?>
 						]);
@@ -206,8 +208,8 @@
 						vAxis: {
 						title: 'Current (Ampere)'
 						},
+						colors: ['#ff2534'],
 						backgroundColor: '#f1f8e9'
-						colors: ['Red']
 						};
 						var chart = new google.visualization.LineChart(document.getElementById('chart_ampere'));
 						chart.draw(data, options);
